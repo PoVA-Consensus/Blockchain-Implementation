2a5dc2207fd7fc8f07ee7ed5274aaf2948571593
lenoah-chacko <lenoahchacko@gmail.com>
2023-04-02 23:08:03 +0530

    Replaced reputation.sol with new logic
    Methods implemented:
    - addNode : Adds a node to the network
    - removeNode : Removes a node from network unless its the last authority
      node.
    - promoteNode (onlyAuthority) : Promote a candidate node to authority
      node
    - demoteNode (onlyAuthority) : Demote authority node (not the last
      authority node)
    - addReputation (onlyAuthority) : adds reputation to node. calls promote
      if reputation crossed
    - removeReputation (onlyAuthority) : removes reputation from node. calls
      demote if reputation drops
    - addBlock (onlyAuthorityNode) : Primary adds a block.
    - rewardPrimary (onlyAuthority) : give block reward to primary authority
    - getNextPrimary : Get the next Primary Authority and reward previous
      primary
    
    To Do:
    - Do we penalize a candidateNode for guessing wrongly?
    - Template to compute blockHash missing
    - call rewardPrimary after verifying blockchain

--- contracts/Reputation.sol
+++ contracts/Reputation.sol
@@ -14,101 +14,156 @@ contract Reputation is ERC20, Ownable {
     ) ERC20("Reputation", "REP") {
         _mint(_owner, _supply);
     }
+    struct Node
+    {
+        bool isAuthority;
+        bool isFullNode;
+        uint256 reputation;
+        uint256 authorityIndex;
+    }
 
-    address[] internal authorityNodes;
+    mapping(address => Node) public nodes;
+    address[] public authorityNodes;
+    uint256 public authorityCount;
+    uint256 public candidateCount;
+    uint256 public blockNumber = 0;
+    address public currentPrimary;
+    uint256 public penaltyThreshold = 50;
+    uint256 public minAuthorityReputation = 1000;
+    uint256 public primaryIndex = 0;
+    uint256 public blockReward = 100;
 
-    // check if node with address _address is authorityNode and return index
-    function isAuthorityNode(
-        address _address
-    ) public view returns (bool, uint256) {
-        for (uint256 s = 0; s < authorityNodes.length; s += 1) {
-            if (_address == authorityNodes[s]) return (true, s);
+    event NodeAdded(address nodeAddress, bool isAuthority, bool isFullNode);
+    event NodeRemoved(address nodeAddress);
+    event NodePromoted(address nodeAddress);
+    event NodeDemoted(address nodeAddress);
+    event NodePenalized(address nodeAddress, uint256 penalty);
+    event BlockAdded(address authorityNode, uint256 blockNumber, bytes32 blockHash);
+    event ReputationAdded(address authorityNode, address nodeAddress, uint256 points);
+    event ReputationRemoved(address authorityNode, address nodeAddress, uint256 points);
+
+    modifier onlyAuthorityNode() {
+        require(nodes[msg.sender].isAuthority, "Only an authority node can call this function");
+        _;
+    }
+
+    function addNode(bool _isAuthority, bool _isFullNode) public {
+        Node storage node = nodes[msg.sender];
+        require(node.reputation == 0, "Node already added");
+        
+        node.isAuthority = _isAuthority;
+        node.isFullNode = _isFullNode;
+        node.reputation = 0;
+        if (_isAuthority) {
+            authorityNodes.push(msg.sender);
+            node.authorityIndex = authorityNodes.length - 1;
+            authorityCount++;
+        } else {
+            candidateCount++;
         }
-        return (false, 0);
+        
+        emit NodeAdded(msg.sender, _isAuthority, _isFullNode);
     }
 
-    //add authorityNode if doesnt exist
-    function addAuthorityNode(address _authorityNode) public {
-        (bool _isAuthorityNode, ) = isAuthorityNode(_authorityNode);
-        if (!_isAuthorityNode) authorityNodes.push(_authorityNode);
-    }
-
-    //remove authorityNode if exists
-    function removeAuthorityNode(address _authorityNode) public {
-        (bool _isAuthorityNode, uint256 s) = isAuthorityNode(_authorityNode);
-        if (_isAuthorityNode) {
-            authorityNodes[s] = authorityNodes[authorityNodes.length - 1];
+    function removeNode(address nodeAddress) public
+    {
+        Node storage node = nodes[nodeAddress];
+        require(node.reputation == 0, "Node must have 0 reputation to be removed");
+        if (node.isAuthority)
+        {
+            require(authorityCount > 1, "Cannot remove last authority node");
+            uint256 index = node.authorityIndex;
+            authorityNodes[index] = authorityNodes[authorityNodes.length - 1];
+            nodes[authorityNodes[index]].authorityIndex = index;
             authorityNodes.pop();
+            authorityCount--;
+        }
+        else
+        {
+            candidateCount--;
+        }
+        delete nodes[msg.sender];
+        emit NodeRemoved(msg.sender);
+    }
+
+    function promoteNode(address nodeAddress) public onlyAuthorityNode
+    {
+        Node storage node = nodes[nodeAddress];
+        require(!node.isAuthority, "Node is already an authority");
+        require(candidateCount > 0, "No candidates to promote");
+        node.isAuthority = true;
+        node.authorityIndex = authorityNodes.length;
+        authorityNodes.push(nodeAddress);
+        candidateCount--;
+        authorityCount++;
+        emit NodePromoted(nodeAddress);
+    }
+
+    function demoteNode(address nodeAddress) public onlyAuthorityNode
+    {
+        Node storage node = nodes[nodeAddress];
+        require(node.isAuthority, "Node is not an authority");
+        require(authorityCount > 1, "Cannot demote last authority node");
+        uint256 index = node.authorityIndex;
+        authorityNodes[index] = authorityNodes[authorityNodes.length - 1];
+        nodes[authorityNodes[index]].authorityIndex = index;
+        authorityNodes.pop();
+        node.isAuthority = false;
+        candidateCount++;
+        authorityCount--;
+        emit NodeDemoted(nodeAddress);
+    }
+
+
+    function addReputation(address nodeAddress, uint256 points) public onlyAuthorityNode
+    {
+        require(points > 0, "Reputation points must be greater than 0");
+        Node storage node = nodes[nodeAddress];
+        node.reputation += points;
+        emit ReputationAdded(msg.sender, nodeAddress, points);
+        if(node.reputation > minAuthorityReputation)
+        {
+            promoteNode(nodeAddress);
         }
     }
 
-    mapping(address => uint256) internal reputationPoints;
-
-    function ReputationPointsOf(
-        address _authorityNode
-    ) public view returns (uint256) {
-        return reputationPoints[_authorityNode];
-    }
-
-    //all reputationPoints sum
-    function totalReputationPoints() public view returns (uint256) {
-        uint256 _totalReputationPoints = 0;
-        for (uint256 s = 0; s < authorityNodes.length; s += 1) {
-            _totalReputationPoints = _totalReputationPoints.add(
-                reputationPoints[authorityNodes[s]]
-            );
+    function removeReputation(address nodeAddress, uint256 points) public onlyAuthorityNode
+    {
+        Node storage node = nodes[nodeAddress];
+        require(node.reputation >= points, "Node does not have enough reputation");
+        if(node.reputation <= points)
+        {
+            node.reputation = 0;
         }
-        return _totalReputationPoints;
-    }
-
-    //delete is reputationPoints exists and create new
-    function createReputationPoints(uint256 _reputationPoints) public {
-        _mint(msg.sender, _reputationPoints);
-        if (reputationPoints[msg.sender] == 0) addAuthorityNode(msg.sender);
-        reputationPoints[msg.sender] = reputationPoints[msg.sender].add(
-            _reputationPoints
-        );
-    }
-
-    function removeReputationPoints(uint256 _reputationPoints) public {
-        reputationPoints[msg.sender] = reputationPoints[msg.sender].sub(
-            _reputationPoints
-        );
-        if (reputationPoints[msg.sender] == 0) removeAuthorityNode(msg.sender);
-        _burn(msg.sender, _reputationPoints);
-    }
-
-    mapping(address => uint256) internal rewards;
-
-    function rewardOf(address _authorityNode) public view returns (uint256) {
-        return rewards[_authorityNode];
-    }
-
-    function totalRewards() public view returns (uint256) {
-        uint256 _totalRewards = 0;
-        for (uint256 s = 0; s < authorityNodes.length; s += 1) {
-            _totalRewards = _totalRewards.add(rewards[authorityNodes[s]]);
+        else
+        {
+            node.reputation -= points;
         }
-        return _totalRewards;
-    }
-
-    function calculateReward(
-        address _authorityNode
-    ) public view returns (uint256) {
-        return reputationPoints[_authorityNode] / 100;
-    }
-
-    function distributeRewards() public onlyOwner {
-        for (uint256 s = 0; s < authorityNodes.length; s += 1) {
-            address authorityNode = authorityNodes[s];
-            uint256 reward = calculateReward(authorityNode);
-            rewards[authorityNode] = rewards[authorityNode].add(reward);
+        if (node.isAuthority && node.reputation < minAuthorityReputation)
+        {
+            demoteNode(nodeAddress);
         }
+            emit ReputationRemoved(msg.sender, nodeAddress, points);
     }
 
-    function withdrawReward() public {
-        uint256 reward = rewards[msg.sender];
-        rewards[msg.sender] = 0;
-        _mint(msg.sender, reward);
+    function addBlock(bytes32 blockHash) public onlyAuthorityNode
+    {
+        require(msg.sender == currentPrimary, "Only primary node can add a block");
+        blockNumber++;
+        emit BlockAdded(msg.sender, blockNumber, blockHash);
+    }
+
+    function rewardPrimary() public onlyAuthorityNode
+    {
+        Node storage node = nodes[currentPrimary];
+        require(node.isAuthority, "Primary node must be an authority");
+        node.reputation += blockReward;
+    }
+
+    function getNextPrimary() public
+    {
+        primaryIndex = (primaryIndex + 1) % authorityNodes.length;
+        currentPrimary = authorityNodes[primaryIndex];
+        rewardPrimary();
     }
 }
